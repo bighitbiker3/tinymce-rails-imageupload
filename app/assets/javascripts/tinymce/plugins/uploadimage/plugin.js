de5007e27e3ea3da2f8c0a895cc2c0e7dcb763f1
Elliott <elliottmcnary@gmail.com>
2020-02-08 17:31:11 -0800

    Change shit

--- app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
+++ app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
@@ -1,30 +1,39 @@
 (function() {
-  tinymce.PluginManager.requireLangPack('uploadimage');
+  tinymce.PluginManager.requireLangPack("uploadimage");
 
-  tinymce.create('tinymce.plugins.UploadImage', {
+  tinymce.create("tinymce.plugins.UploadImage", {
     UploadImage: function(ed, url) {
       var form,
         iframe,
         win,
         throbber,
-        selectedClass = '',
+        selectedClass = "",
         editor = ed;
 
       function showDialog() {
         var classList = getClassList();
         var body = [
-          { type: 'iframe', url: 'javascript:void(0)' },
-          { type: 'textbox', name: 'file', label: ed.translate('Choose an image'), subtype: 'file' },
-          { type: 'textbox', name: 'alt', label: ed.translate('Image description') }
+          { type: "iframe", url: "javascript:void(0)" },
+          {
+            type: "textbox",
+            name: "file",
+            label: ed.translate("Choose an image"),
+            subtype: "file"
+          },
+          {
+            type: "textbox",
+            name: "alt",
+            label: ed.translate("Image description")
+          }
         ];
 
         if (classList.length > 0) {
           selectedClass = classList[0].value;
           body = body.concat([
             {
-              type: 'listbox',
-              name: 'class',
-              label: ed.translate('Class'),
+              type: "listbox",
+              name: "class",
+              label: ed.translate("Class"),
               values: classList,
               onSelect: function(e) {
                 selectedClass = this.value();
@@ -34,26 +43,32 @@
         }
 
         body = body.concat([
-          { type: 'container', classes: 'error', html: "<p style='color: #b94a48;'>&nbsp;</p>" },
+          {
+            type: "container",
+            classes: "error",
+            html: "<p style='color: #b94a48;'>&nbsp;</p>"
+          },
 
           // Trick TinyMCE to add a empty div that "preloads" the throbber image
-          { type: 'container', classes: 'throbber' }
+          { type: "container", classes: "throbber" }
         ]);
 
         win = editor.windowManager.open(
           {
-            title: ed.translate('Insert an image from your computer'),
-            width: 520 + parseInt(editor.getLang('uploadimage.delta_width', 0), 10),
-            height: 180 + parseInt(editor.getLang('uploadimage.delta_height', 0), 10),
+            title: ed.translate("Insert an image from your computer"),
+            width:
+              520 + parseInt(editor.getLang("uploadimage.delta_width", 0), 10),
+            height:
+              180 + parseInt(editor.getLang("uploadimage.delta_height", 0), 10),
             body: body,
             buttons: [
               {
-                text: ed.translate('Insert'),
+                text: ed.translate("Insert"),
                 onclick: insertImage,
-                subtype: 'primary'
+                subtype: "primary"
               },
               {
-                text: ed.translate('Cancel'),
+                text: ed.translate("Cancel"),
                 onclick: ed.windowManager.close
               }
             ]
@@ -64,13 +79,13 @@
         );
 
         /* WHY DO YOU HATE <form>, TINYMCE!? */
-        iframe = win.find('iframe')[0];
-        form = createElement('form', {
-          action: ed.getParam('uploadimage_form_url', '/tinymce_assets'),
+        iframe = win.find("iframe")[0];
+        form = createElement("form", {
+          action: ed.getParam("uploadimage_form_url", "/tinymce_assets"),
           target: iframe._id,
-          method: 'POST',
-          enctype: 'multipart/form-data',
-          accept_charset: 'UTF-8'
+          method: "POST",
+          enctype: "multipart/form-data",
+          accept_charset: "UTF-8"
         });
 
         // Might have several instances on the same page,
@@ -78,38 +93,53 @@
         iframe.getEl().name = iframe._id;
 
         // Create some needed hidden inputs
-        form.appendChild(createElement('input', { type: 'hidden', name: 'utf8', value: '✓' }));
         form.appendChild(
-          createElement('input', { type: 'hidden', name: 'authenticity_token', value: getMetaContents('csrf-token') })
+          createElement("input", { type: "hidden", name: "utf8", value: "✓" })
         );
-        form.appendChild(createElement('input', { type: 'hidden', name: hintName(), value: hintValue() }));
+        form.appendChild(
+          createElement("input", {
+            type: "hidden",
+            name: "authenticity_token",
+            value: getMetaContents("csrf-token")
+          })
+        );
+        form.appendChild(
+          createElement("input", {
+            type: "hidden",
+            name: hintName(),
+            value: hintValue()
+          })
+        );
 
         var el = win.getEl();
-        var body = document.getElementById(el.id + '-body');
+        var body = document.getElementById(el.id + "-body");
 
         // Copy everything TinyMCE made into our form
-        var containers = body.getElementsByClassName('mce-container');
+        var containers = body.getElementsByClassName("mce-container");
         for (var i = 0; i < containers.length; i++) {
           form.appendChild(containers[i]);
         }
 
         // Fix inputs, since TinyMCE hates HTML and forms
-        var inputs = form.getElementsByTagName('input');
+        var inputs = form.getElementsByTagName("input");
         for (var i = 0; i < inputs.length; i++) {
           var ctrl = inputs[i];
 
-          if (ctrl.tagName.toLowerCase() === 'input' && ctrl.type !== 'hidden') {
-            if (ctrl.type === 'file') {
-              ctrl.name = inputName('file');
+          if (
+            ctrl.tagName.toLowerCase() === "input" &&
+            ctrl.type !== "hidden"
+          ) {
+            if (ctrl.type === "file") {
+              ctrl.name = inputName("file");
 
               // Hack styles
               tinymce.DOM.setStyles(ctrl, {
                 border: 0,
-                boxShadow: 'none',
-                webkitBoxShadow: 'none'
+                boxShadow: "none",
+                webkitBoxShadow: "none"
               });
             } else {
-              ctrl.name = inputName('alt');
+              ctrl.name = inputName("alt");
             }
           }
         }
@@ -118,24 +148,24 @@
       }
 
       function hintName() {
-        return inputName(ed.getParam('uploadimage_hint_key', 'hint'));
+        return inputName(ed.getParam("uploadimage_hint_key", "hint"));
       }
 
       function hintValue() {
-        return ed.getParam('uploadimage_hint', '');
+        return ed.getParam("uploadimage_hint", "");
       }
 
       function inputName(name) {
-        if (ed.getParam('uploadimage_model', false)) {
-          return ed.getParam('uploadimage_model') + '[' + name + ']';
+        if (ed.getParam("uploadimage_model", false)) {
+          return ed.getParam("uploadimage_model") + "[" + name + "]";
         } else {
           return name;
         }
       }
 
       function insertImage() {
-        if (getInputValue(inputName('file')) === '') {
-          return handleError('You must choose a file');
+        if (getInputValue(inputName("file")) === "") {
+          return handleError("You must choose a file");
         }
 
         throbber = new top.tinymce.ui.Throbber(win.getEl());
@@ -149,11 +179,11 @@
          */
         var target = iframe.getEl();
         if (target.attachEvent) {
-          target.detachEvent('onload', uploadDone);
-          target.attachEvent('onload', uploadDone);
+          target.detachEvent("onload", uploadDone);
+          target.attachEvent("onload", uploadDone);
         } else {
-          target.removeEventListener('load', uploadDone);
-          target.addEventListener('load', uploadDone, false);
+          target.removeEventListener("load", uploadDone);
+          target.addEventListener("load", uploadDone, false);
         }
 
         form.submit();
@@ -167,10 +197,10 @@
           var target = iframe.getEl();
           if (target.document || target.contentDocument) {
             var doc = target.contentDocument || target.contentWindow.document;
-            if (String(doc.contentType).indexOf('html') > -1) {
-              handleResponse(doc.getElementsByTagName('body')[0].innerHTML);
+            if (String(doc.contentType).indexOf("html") > -1) {
+              handleResponse(doc.getElementsByTagName("body")[0].innerHTML);
             } else {
-              handleResponse(doc.getElementsByTagName('pre')[0].innerHTML);
+              handleResponse(doc.getElementsByTagName("pre")[0].innerHTML);
             }
           }
         } catch (e) {
@@ -182,30 +212,30 @@
         try {
           var json = tinymce.util.JSON.parse(ret);
 
-          if (json['error']) {
-            handleError(json['error']['message']);
+          if (json["error"]) {
+            handleError(json["error"]["message"]);
           } else {
-            ed.execCommand('mceInsertContent', false, buildHTML(json));
+            ed.execCommand("mceInsertContent", false, buildHTML(json));
             ed.windowManager.close();
           }
         } catch (e) {
-          handleError('Got a bad response from the server');
+          handleError("Got a bad response from the server");
         }
       }
 
       function clearErrors() {
-        var message = win.find('.error')[0].getEl();
+        var message = win.find(".error")[0].getEl();
 
         if (message) {
-          message.getElementsByTagName('p')[0].innerHTML = '&nbsp;';
+          message.getElementsByTagName("p")[0].innerHTML = "&nbsp;";
         }
       }
 
       function handleError(error) {
-        var message = win.find('.error')[0].getEl();
+        var message = win.find(".error")[0].getEl();
 
         if (message) {
-          message.getElementsByTagName('p')[0].innerHTML = ed.translate(error);
+          message.getElementsByTagName("p")[0].innerHTML = ed.translate(error);
         }
       }
 
@@ -221,42 +251,45 @@
       }
 
       function buildHTML(json) {
-        var image = json[ed.getParam('uploadimage_model', 'image')];
-        var defaultClass = ed.getParam('uploadimage_default_img_class', '');
-        var figure = ed.getParam('uploadimage_figure', false);
-        var altText = getInputValue(inputName('alt'));
+        var image = json[ed.getParam("uploadimage_model", "image")];
+        var defaultClass = ed.getParam("uploadimage_default_img_class", "");
+        var figure = ed.getParam("uploadimage_figure", false);
+        var altText = getInputValue(inputName("alt"));
 
-        var imgstr = "<img src='" + image['url'] + "'";
+        var imgstr = "<img src='" + image["url"] + "'";
 
-        if (defaultClass !== '') {
-          imgstr += " class='" + defaultClass + ' ' + selectedClass + "'";
+        if (defaultClass !== "") {
+          imgstr += " class='" + defaultClass + " " + selectedClass + "'";
         }
 
-        if (image['height']) {
-          imgstr += " height='" + image['height'] + "'";
+        if (image["height"]) {
+          imgstr += " height='" + image["height"] + "'";
         }
-        if (image['width']) {
-          imgstr += " width='" + image['width'] + "'";
+        if (image["width"]) {
+          imgstr += " width='" + image["width"] + "'";
         }
 
         imgstr += " alt='" + altText + "'/>";
 
         if (figure) {
-          var figureClass = ed.getParam('uploadimage_figure_class', 'figure');
-          var figcaptionClass = ed.getParam('uploadimage_figcaption_class', 'figcaption');
+          var figureClass = ed.getParam("uploadimage_figure_class", "figure");
+          var figcaptionClass = ed.getParam(
+            "uploadimage_figcaption_class",
+            "figcaption"
+          );
 
-          var figstr = '<figure';
+          var figstr = "<figure";
 
-          if (figureClass !== '') {
+          if (figureClass !== "") {
             figstr += " class='" + figureClass + "'";
           }
-          figstr += '>' + imgstr;
-          figstr += '<figcaption';
-          if (figcaptionClass !== '') {
+          figstr += ">" + imgstr;
+          figstr += "<figcaption";
+          if (figcaptionClass !== "") {
             figstr += " class='" + figcaptionClass + "'";
           }
-          figstr += '>' + altText + '</figcaption>';
-          figstr += '</figure>';
+          figstr += ">" + altText + "</figcaption>";
+          figstr += "</figure>";
 
           return figstr;
         } else {
@@ -265,7 +298,7 @@
       }
 
       function getInputValue(name) {
-        var inputs = form.getElementsByTagName('input');
+        var inputs = form.getElementsByTagName("input");
 
         for (var i in inputs) {
           if (inputs[i].name === name) {
@@ -273,11 +306,11 @@
           }
         }
 
-        return '';
+        return "";
       }
 
       function getMetaContents(mn) {
-        var m = document.getElementsByTagName('meta');
+        var m = document.getElementsByTagName("meta");
 
         for (var i in m) {
           if (m[i].name === mn) {
@@ -289,35 +322,35 @@
       }
 
       function getClassList() {
-        var config = ed.getParam('image_class_list', []);
+        var config = ed.getParam("image_class_list", []);
         var values = [];
         for (var i = 0; i < config.length; i++) {
           values[i] = {
-            text: config[i]['title'],
-            value: config[i]['value']
+            text: config[i]["title"],
+            value: config[i]["value"]
           };
         }
         return values;
       }
 
-      if (editor.getParam('uploadimage', true)) {
+      if (editor.getParam("uploadimage", true)) {
         // Add a button that opens a window
-        editor.addButton('uploadimage', {
-          tooltip: ed.translate('Insert an image from your computer'),
-          icon: 'image',
+        editor.ui.registry.addButton("uploadimage", {
+          tooltip: ed.translate("Insert an image from your computer"),
+          icon: "image",
           onclick: showDialog
         });
 
         // Adds a menu item to the tools menu
-        editor.addMenuItem('uploadimage', {
-          text: ed.translate('Insert an image from your computer'),
-          icon: 'image',
-          context: 'insert',
+        editor.addMenuItem("uploadimage", {
+          text: ed.translate("Insert an image from your computer"),
+          icon: "image",
+          context: "insert",
           onclick: showDialog
         });
       }
     }
   });
 
-  tinymce.PluginManager.add('uploadimage', tinymce.plugins.UploadImage);
+  tinymce.PluginManager.add("uploadimage", tinymce.plugins.UploadImage);
 })();
